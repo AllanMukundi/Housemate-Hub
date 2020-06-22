6d241cff9a95f2cb83fe0414ac0c4d81f55f6b83
Michelle Dang <michelle.dang3929@gmail.com>
2020-06-22 16:13:47 -0400

    create bottom nav ui

--- app/src/main/java/com/cs446/housematehub/LoggedInBaseActivity.java
+++ app/src/main/java/com/cs446/housematehub/LoggedInBaseActivity.java
@@ -2,15 +2,20 @@ package com.cs446.housematehub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.cs446.housematehub.userinit.LoginActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
@@ -21,8 +26,38 @@ public abstract class LoggedInBaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_logged_in_base);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.navigationView);
+        bottomNavigation.bringToFront();
+        bottomNavigation.setSelectedItemId(R.id.nav_home);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.d("SELECTED ITEM", "selected item");
+                switch (item.getItemId()) {
+                    case R.id.nav_expenses:
+                        // Switch to expenses view here
+                        break;
+                    case R.id.nav_calendar:
+                        // Switch to calendar view here
+                        break;
+                    case R.id.nav_home:
+                        // Switch to dashboard view here
+                        break;
+                    case R.id.nav_chat:
+                        // Switch to chat view here
+                        break;
+                    case R.id.nav_lists:
+                        // Switch to lists view here
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     protected void initSpinner(Spinner spinner) {
