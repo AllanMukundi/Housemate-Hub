839f888aafdef4b5ad87a1ff8ac4fd040ff430ab
Michelle Dang <michelle.dang3929@gmail.com>
2020-06-24 20:33:55 -0400

    move navbar code to HouseMainActivity

--- app/src/main/java/com/cs446/housematehub/HouseMainActivity.java
+++ app/src/main/java/com/cs446/housematehub/HouseMainActivity.java
@@ -1,7 +1,12 @@
 package com.cs446.housematehub;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HouseMainActivity extends LoggedInBaseActivity {
 
@@ -13,5 +18,34 @@ public class HouseMainActivity extends LoggedInBaseActivity {
         setContentView(R.layout.activity_house_main);
         spinner = findViewById(R.id.menu);
         super.initSpinner(spinner);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.navigationView);
+        bottomNavigation.bringToFront();
+        bottomNavigation.setSelectedItemId(R.id.nav_home);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
     }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_expenses:
+                    // Switch to expenses view here
+                    break;
+                case R.id.nav_calendar:
+                    // Switch to calendar view here
+                    break;
+                case R.id.nav_home:
+                    // Switch to dashboard view here
+                    break;
+                case R.id.nav_chat:
+                    // Switch to chat view here
+                    break;
+                case R.id.nav_lists:
+                    // Switch to lists view here
+                    break;
+            }
+            return true;
+        }
+    };
 }
--- app/src/main/java/com/cs446/housematehub/LoggedInBaseActivity.java
+++ app/src/main/java/com/cs446/housematehub/LoggedInBaseActivity.java
@@ -2,18 +2,15 @@ package com.cs446.housematehub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.cs446.housematehub.userinit.LoginActivity;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
@@ -24,37 +21,8 @@ public abstract class LoggedInBaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_logged_in_base);
-
         final Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        BottomNavigationView bottomNavigation = findViewById(R.id.navigationView);
-        bottomNavigation.bringToFront();
-        bottomNavigation.setSelectedItemId(R.id.nav_home);
-        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.nav_expenses:
-                        // Switch to expenses view here
-                        break;
-                    case R.id.nav_calendar:
-                        // Switch to calendar view here
-                        break;
-                    case R.id.nav_home:
-                        // Switch to dashboard view here
-                        break;
-                    case R.id.nav_chat:
-                        // Switch to chat view here
-                        break;
-                    case R.id.nav_lists:
-                        // Switch to lists view here
-                        break;
-                }
-                return true;
-            }
-        });
     }
 
     protected void initSpinner(Spinner spinner) {
