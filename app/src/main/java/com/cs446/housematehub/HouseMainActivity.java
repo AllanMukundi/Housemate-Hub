402c55cd9357ccb27cb067b4d6a385ac1ad22af6
Walter Mao <waltermao98@gmail.com>
2020-08-02 17:01:30 -0700

    Remove toolbar spinner with single log out option. There's now a cleaner way to log out: Dashboard -> Account -> Log Out

--- app/src/main/java/com/cs446/housematehub/HouseMainActivity.java
+++ app/src/main/java/com/cs446/housematehub/HouseMainActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -75,8 +74,6 @@ public class HouseMainActivity extends LoggedInBaseActivity {
             }
         });
 
-        Spinner spinner = findViewById(R.id.menu);
-        super.initSpinner(spinner);
         currentUser = ParseUser.getCurrentUser();
         String houseName = currentUser.get("houseName").toString();
         currentHouse = getCurrentHouse(houseName);
